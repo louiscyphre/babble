114aa1a18bac993f75daab8fb9637a7a60d356c2
louiscyphre <dev.cccp@gmail.com>
2017-08-14 01:13:10 +0300

    Added initial test files

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,79 +1,123 @@
-(function (global) {
+/* jshint -W097 */
+'use strict';
+/* global require, console, document, XMLHttpRequest, FormData, Promise,localStorage */
+console.log('hello from client');
 
-    /*  'use strict';
-       global require, console, document,XMLHttpRequest 
-      console.log('hello from client');
-      var form = document.getElementsByClassName('Chat-sendMessageForm');
+var Babble = {
 
-      form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        console.log(form.action);
-        var data = '';
-        for (var element in form.elements) {
-          if (element.name) {
-            data += element.name + '=' + encodeURIComponent(element.value) + '&';
-          }
-        }
-        var xhr = new XMLHttpRequest();
-        xhr.open(form.method, form.action);
-        if (form.method === 'POST') {
-          xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        }
-        xhr.addEventListener('load', function (e) {
-          console.log(e.target.responseText);
-        });
-        xhr.send(data);
-      });*/
-
-    'use strict';
-    /* global require, console, document, XMLHttpRequest, Promise */
-    console.log('hello from client');
-
-    var form = document.querySelector('form');
-
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        request({
-            method: form.method,
-            action: form.action,
-            data: serialize(form)
-        }).then(function (result) {
-            console.log(result);
-        });
-    });
-
-    function serialize(form) {
-        return form.elements.reduce(function (data, element) {
-            if (element.name) {
-                data += element.name + '=' + encodeURIComponent(element.value) + '&';
-            }
-            return data;
-        }, '');
-    }
-
-    function request(options) {
+    request: function request(options) {
         /* jshint -W098 */
         return new Promise(function (resolve, reject) {
             var xhr = new XMLHttpRequest();
             xhr.open(options.method, options.action);
-            if (options.method === 'post') {
-                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            }
             xhr.addEventListener('load', function (e) {
                 resolve(e.target.responseText);
             });
             xhr.send(options.data);
         });
-    }
+    },
+    run: function (document) {
 
-    makeGrowable(document.querySelector('.js-growable'));
+        /*var formCallback = function formCallback(e) {
+            e.preventDefault();
+            Babble.request({
+                method: e.target.method,
+                action: e.target.action,
+                data: serialize(e.target)
+            }).then(function (result) {
+                console.log(result);
+            });
+        }
 
-    function makeGrowable(container) {
-        var area = container.querySelector('textarea');
-        var clone = container.querySelector('span');
-        area.addEventListener('input', function (e) {
-            clone.textContent = area.value;
+        var newMessageForm = document.querySelector('.Chat-sendMessageForm');
+
+        newMessageForm.addEventListener('submit', formCallback);
+
+        var registerForm = document.querySelector('.Modal');
+
+        registerForm.addEventListener('submit', formCallback);*/
+
+        var newMessageForm = document.querySelector('.Chat-sendMessageForm');
+
+        newMessageForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+            Babble.request({
+                method: newMessageForm.method,
+                action: newMessageForm.action,
+                data: serialize(newMessageForm)
+            }).then(function (result) {
+                console.log(result);
+            });
         });
-    }
 
-}(this.window));
+        var registerForm = document.querySelector('.Modal');
+
+        registerForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+            var userInfo = {
+                fullname: registerForm.elements[0].value,
+                email: registerForm.elements[1].value
+            };
+            Babble.register(serialize(registerForm))
+                .then(function (result) {
+                    console.log(result);
+                    //var userInfo = new FormData(registerForm);
+                    localStorage.setItem("babble", JSON.stringify(userInfo));
+                });
+        });
+        //var modal = document.querySelector('.Modal');
+
+        //var close = document.querySelector('.Modal--close');
+
+        //close.onclick = function () {
+        //    modal.style.display = "none";
+        //};
+
+        function serialize(form) {
+            var data = '';
+            //console.log('enter serialize: ');
+
+            for (var i = 0; i < form.elements.length; i++) {
+                var element = form.elements[i];
+                //console.log("form.elements[i]:", JSON.stringify(JSON.parse(form.elements[i])));
+                if (element.name) {
+                    data += element.name + '=' + encodeURIComponent(element.value) + '&';
+
+                    //console.log('serialize: ', element.name);
+                }
+            }
+            console.log('encoded data: ', data);
+            return data;
+        }
+
+        /*function serialize(form) {
+            console.log(new FormData(form));
+            return new FormData(form);
+        }*/
+
+
+        makeGrowable(document.querySelector('.js-growable'));
+
+        function makeGrowable(container) {
+            var area = container.querySelector('textarea');
+            var clone = container.querySelector('span');
+            area.addEventListener('input', function (e) {
+                clone.textContent = area.value;
+            });
+        }
+    },
+
+    register: function register(props) {
+        return Babble.request({
+            method: "POST",
+            action: "http://localhost:8000/login",
+            data: props
+        });
+    },
+
+    postMessage: function postMessage(message, callback) {
+        return Babble.request(message);
+    }
+};
+
+Babble.run(document);
--- server/main.js
+++ server/main.js
@@ -1,46 +1,47 @@
 (function (global) {
 
-  'use strict';
-  /* global require, console */
-  var http = require('http');
-  var urlUtil = require('url');
-  var queryUtil = require('querystring');
-  var server = http.createServer(function (request, response) {
+    'use strict';
+    /* global require, console */
+    var http = require('http');
+    var urlUtil = require('url');
+    var queryUtil = require('querystring');
+    var server = http.createServer(function (request, response) {
 
-    response.setHeader('Access-Control-Allow-Origin', '*');
+        response.setHeader('Access-Control-Allow-Origin', '*');
 
-    if (request.method === 'GET') {
+        if (request.method === 'GET') {
 
-      var url = urlUtil.parse(request.url);
-      var data = queryUtil.parse(url.query);
-      console.log(data.message);
+            var url = urlUtil.parse(request.url);
+            var data = queryUtil.parse(url.query);
+            console.log(data.message);
 
-      if (!data.message) {
-        response.writeHead(400);
-      }
-      response.end();
+            if (!data.message) {
+                response.writeHead(400);
+            }
+            response.end();
 
-    } else if (request.method === 'POST') {
+        } else if (request.method === 'POST') {
 
-      var requestBody = 'dfadfasd';
+            var requestBody = '';
 
-      request.on('data', function (chunk) {
-        requestBody += chunk;
-      });
+            request.on('data', function (chunk) {
+                requestBody += chunk;
+            });
 
-      request.on('end', function () {
-        var data = queryUtil.parse(requestBody);
-        console.log('we have all the data ', data.message);
-        response.end('thank you');
-      });
+            request.on('end', function () {
+                var data = queryUtil.parse(requestBody);
+                //console.log('we have all the data ', data);
+                console.log('we have all the data ', data);
+                response.end('thank you');
+            });
 
-    } else {
-      response.writeHead(405);
-      response.end();
-    }
-  });
-  server.listen(8000);
-  console.log("Cou cou ca vah");
+        } else {
+            response.writeHead(405);
+            response.end();
+        }
+    });
+    server.listen(8000);
+    console.log("Cou cou ca vah");
 
 
 }(this.window));
